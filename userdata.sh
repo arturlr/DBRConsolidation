b35316da61e5609b6feeda54e30a752ca45d9704
Artur Rodrigues <arturlr@live.com>
2017-03-30 22:52:28 +0000

    v03301852

--- userdata.sh
+++ userdata.sh
@@ -3,15 +3,24 @@
 # INSTALL PRE-REQS
 sudo yum update -y
 sudo chmod 777 /media/ephemeral0/
-sudo yum install -y git
 sudo mkdir /opt/drill
 sudo mkdir /opt/drill/log
 sudo chmod 777 /opt/drill/log
-sudo curl -s "http://download.nextag.com/apache/drill/drill-1.9.0/apache-drill-1.9.0.tar.gz" | sudo tar xz --strip=1 -C /opt/drill
 sudo yum install -y java-1.8.0-openjdk
+sudo curl -s "http://download.nextag.com/apache/drill/drill-1.9.0/apache-drill-1.9.0.tar.gz" | sudo tar xz --strip=1 -C /opt/drill
 sudo yum install -y python35
+sudo yum install -y python35-devel
+curl -O https://bootstrap.pypa.io/get-pip.py
+python3 get-pip.py --user
+~/.local/bin/pip install -U pip
+sudo yum install -y gcc
+sudo yum install -y gcc-c++
 sudo yum install -y aws-cli
 sudo yum install -y unzip
+sudo yum install -y git
+
+# Prep Libraries
+~/.local/bin/pip install PyAthenaJDBC
 
 # Get Files
 cd ~
