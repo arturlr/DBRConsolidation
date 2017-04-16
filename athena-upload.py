45329d804ef2eac0382762017050f7e7f3cad40e
artdev <arturlr@live.com>
2017-04-16 18:35:02 -0400

    v170416-2102

--- athena-upload.py
+++ athena-upload.py
@@ -39,10 +39,9 @@ class CloudWatch:
         self.cwclient = boto3.client('cloudwatch')
 
     def send_metrics(self, dimensions, timestamp, metricname, value, unit):
-        MetricData = []
-        MetricData.append({'MetricName':metricname,'Dimensions':dimensions,'Value':value,'Unit':unit})
+        MetricData = {'MetricName':metricname,'Dimensions':dimensions,'Value':value,'Unit':unit}
         if (timestamp.year == datetime.today().year):
-            MetricData.append({'Timestamp': timestamp})
+            MetricData['Timestamp'] = timestamp
 
         print(MetricData)
         self.cwclient.put_metric_data(
