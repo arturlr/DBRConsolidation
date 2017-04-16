9567094535a8daaf04aff2186236de961e353143
artdev <arturlr@live.com>
2017-04-16 10:27:36 -0400

    v041617-05

--- athena-upload.py
+++ athena-upload.py
@@ -34,8 +34,8 @@ class Athena:
 
 class CloudWatch:
     def __init__(self):
-        #CW_NAMESPACE = "DBRconsolidation"
-        CW_NAMESPACE = "DBRtest"
+        #self.CW_NAMESPACE = "DBRconsolidation"
+        self.CW_NAMESPACE = "DBRtest"
         self.connection = boto3.client('cloudwatch')
 
     def send_metrics(self, dimensions, timestamp, metricname, value, unit):
