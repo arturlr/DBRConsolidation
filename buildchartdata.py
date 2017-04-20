ee667837efbeaaae75a29b6849a67295c4d1e419
artdev <arturlr@live.com>
2017-04-20 10:25:56 -0400

    v170420-1025

--- buildchartdata.py
+++ buildchartdata.py
@@ -13,59 +13,114 @@ import aws.classes
 
 
 class BuildChartData:
-    def __init__(self):
+    def __init__(self,access_key,secret_key):
         self.filenamepath = "/media/ephemeral0/"
         self.cw = aws.classes.CloudWatch()
+        self.date_suffix_athena = datetime.now().strftime("%Y_%m")
 
-    def get_total_month_to_date(self, payer, cwname, dimensions):
+        client = boto3.client("sts", aws_access_key_id=access_key, aws_secret_access_key=secret_key)
+        current_account_id = client.get_caller_identity()["Account"]
+        self.ath = aws.classes.Athena(access_key, secret_key, "us-east-1", current_account_id)
+
+    def get_last_6_month_to_date(self, payer_accounts, metric_name):
         # Get Month-to-Date payer billing info
         period = 86400
-        title = dimensions[len(dimensions)-1]['Value']
-        #for i in range(0,len(dimensions)):
-        #    title = title + dimensions[i]['Value']
-        #    title = title + " - "
 
-        c3_data_arr = [title]
-        for f in range(0, 5):
-            dtref = datetime.utcnow() - relativedelta(months=+f + 1)
-            month = dtref.month
-            year = dtref.year
-            num_days = calendar.monthrange(year, month)
-            startdt = datetime(year, month, 1)
-            enddt = datetime(year, month, num_days[1])
+        c3_data = [{'x': ['x']}]
+        c3_data_index = 0
 
-            #print(startdt.strftime('%d-%m-%Y'))
-            #print(enddt.strftime('%d-%m-%Y'))
-            rsp = self.cw.get_metrics(dimensions, cwname, startdt, enddt, period, 'Maximum')
-            c3_data_arr.append(self.get_maximum_datapoint(rsp['Datapoints']))
+        # Populate with the date range
+        for f in range(0, 6):
+            dt_ref = datetime.utcnow() - relativedelta(months=+f + 1)
+            month = dt_ref.month
+            year = dt_ref.year
+            dt_start = datetime(year, month, 1)
+            c3_data[c3_data_index]['x'].append(dt_start.strftime('%m-%Y'))
 
-        enddt = datetime.utcnow()
-        startdt = datetime(enddt.year, enddt.month, 1)
-        rsp = self.cw.get_metrics(dimensions, cwname, startdt, enddt, period, 'Maximum')
-        c3_data_arr.append(self.get_maximum_datapoint(rsp['Datapoints']))
-        print(c3_data_arr)
+        for payer in payeraccounts:
+            dimensions_arr = [{'Name': 'PayerAccountId', 'Value': payer}]
+            c3_data.append({'data': [payer]})
+            c3_data_index = c3_data_index + 1
 
-    def get_per_hour(self, payer, cwname, dimensions):
-        # Get Month-to-Date payer billing info
+            for f in range(0, 5):
+                dt_ref = datetime.utcnow() - relativedelta(months=+f + 1)
+                month = dt_ref.month
+                year = dt_ref.year
+                num_days = calendar.monthrange(year, month)
+                dt_start = datetime(year, month, 1)
+                dt_end = datetime(year, month, num_days[1])
+
+                rsp = self.cw.get_metrics(dimensions_arr, metric_name, dt_start, dt_end, period, 'Maximum')
+                c3_data[c3_data_index]['data'].append(self.get_maximum_datapoint(rsp['Datapoints']))
+
+            dt_end = datetime.utcnow()
+            dt_start = datetime(dt_end.year, dt_end.month, 1)
+            rsp = self.cw.get_metrics(dimensions_arr, metric_name, dt_start, dt_end, period, 'Maximum')
+            c3_data[c3_data_index]['data'].append(self.get_maximum_datapoint(rsp['Datapoints']))
+
+        return c3_data
+
+    def get_services(self, payer_account, metric_name, range):
+        if range == 'daily':
+            period = 86400
+            dt_end = datetime.utcnow()
+            dt_start = datetime(dt_end.year, dt_end.month, 1)
+        else:
+            period = 600
+            dt_end = datetime.utcnow()
+            dt_start = datetime.utcnow() - timedelta(days=1)
+
+        list_svc = []
+
+        c3_data = [{'x': ['x']}]
+        c3_data_index = 0
+
+        # Consolidating all the services into one list
+        for payer in payeraccounts:
+            query = "SELECT DISTINCT productname FROM dbr.autodbr_%s_%s" % (payer, self.date_suffix_athena)
+            rst = self.ath.Request(query)
+            for s in rst['rows']:
+                if s['productname'] == '':
+                    continue
+                if s['productname'] not in list_svc:
+                    list_svc.append(s['productname'])
+
+        for payer in payeraccounts:
+            for svc in list_svc:
+                dimensions_arr = [{'Name': 'PayerAccountId', 'Value': payer},
+                                  {'Name': 'ProductName', 'Value': svc}]
+
+                if range == 'daily':
+                    rsp = self.cw.get_metrics(dimensions_arr, metric_name, dt_start, dt_end, period, 'Maximum')
+                else:
+                    rsp = self.cw.get_metrics(dimensions_arr, metric_name, dt_start, dt_end, period, 'Maximum')
+                    hours_sorted = sorted(rsp['Datapoints'], key=lambda k: k['Timestamp'])
+
+                    for h in hours_sorted:
+                        c3_data[0]['x'].append(h['Timestamp'].strftime('%H-%M'))
+                        c3_data[c3_data_index]['data'].append(h['Maximum'])
+
+
+    def get_per_hour(self, payer_accounts, metric_name):
         period = 600
-        title = dimensions[len(dimensions)-1]['Value']
-        #for i in range(0,len(dimensions)):
-        #    title = title + dimensions[i]['Value']
-        #    title = title + " - "
 
-        c3_data_arr = [title]
-        c3_x_arr = ['x']
-        startdt = datetime.utcnow() - timedelta(days=1)
-        enddt = datetime.utcnow()
-        rsp = self.cw.get_metrics(dimensions, cwname, startdt, enddt, period, 'Maximum')
-        hours_sorted = sorted(rsp['Datapoints'], key=lambda k: k['Timestamp'])
+        c3_data = [{'x': ['x']}]
+        c3_data_index = 0
+        for payer in payeraccounts:
+            c3_data.append({'data': [payer]})
+            c3_data_index = c3_data_index + 1
 
-        for h in hours_sorted:
-            c3_x_arr.append(h['Timestamp'].strftime('%H-%M'))
-            c3_data_arr.append(h['Maximum'])
+            dimensions_arr = [{'Name': 'PayerAccountId', 'Value': payer}]
+            dt_start = datetime.utcnow() - timedelta(days=1)
+            dt_end = datetime.utcnow()
+            rsp = self.cw.get_metrics(dimensions_arr, metric_name, dt_start, dt_end, period, 'Maximum')
+            hours_sorted = sorted(rsp['Datapoints'], key=lambda k: k['Timestamp'])
 
-        print(c3_x_arr)
-        print(c3_data_arr)
+            for h in hours_sorted:
+                c3_data[0]['x'].append(h['Timestamp'].strftime('%H-%M'))
+                c3_data[c3_data_index]['data'].append(h['Maximum'])
+
+        return c3_data
 
     def get_maximum_datapoint(self,datapoints):
         maxpt = 0
@@ -76,47 +131,36 @@ class BuildChartData:
 
         return maxpt
 
-
-
-# AWS AccountID
-reg = re.compile(r'([0-9]{12})', re.I)
-
-today = datetime.today()
-date_suffix_athena = today.strftime("%Y_%m")
-date_suffix_bucket = today.strftime("%Y-%m")
+###################################
+#
+# Initialize Variable
 
 if len(sys.argv) != 4:
     print('Invalid arguments. Please use aws_key aws_secret payeraccountid;payeraccountid;...')
     sys.exit(1)
 
-aws_access_key = sys.argv[1]
-aws_secret_key = sys.argv[2]
+
 payeraccounts = sys.argv[3].split(';')
 
-#print(payeraccounts)
+bch = BuildChartData(sys.argv[1],sys.argv[2])
 
-client = boto3.client("sts", aws_access_key_id=sys.argv[1], aws_secret_access_key=sys.argv[2])
-current_account_id = client.get_caller_identity()["Account"]
+###################################
+#
+# Obtain Data and Creating the report arrays
 
-ath = aws.classes.Athena(aws_access_key, aws_secret_key, "us-east-1", current_account_id)
-bch = BuildChartData()
+metric_name = 'Estimate Month-to-Date Payer'
+response = bch.get_last_6_month_to_date(payeraccounts,metric_name)
+c3_data = [{metric_name: response }]
 
-# Getting data and collecting metrics.
-for payer in payeraccounts:
-    # Total Month-to-Date
-    dimensions_arr = [{'Name': 'PayerAccountId', 'Value': payer}]
-    response = bch.get_total_month_to_date(payer,'Total Month-to-Date Payer',dimensions_arr)
+metric_name = 'Estimate Per Hour Payer'
+response = bch.get_per_hour(payeraccounts,metric_name)
+c3_data.append({metric_name:[response]})
 
-    response = bch.get_per_hour(payer, 'Total Per Hour Payer', dimensions_arr)
+metric_name = 'Estimate Month-to-Date Services'
+response = bch.get_services(payeraccounts, metric_name,'daily')
+c3_data.append({metric_name:[response]})
 
-    # Service Month-to-Date
-    query = "SELECT DISTINCT productname FROM dbr.autodbr_%s_%s" % (payer,date_suffix_athena)
-    rst = ath.Request(query)
-    for svc in rst['rows']:
-        if svc['productname'] == '':
-            continue
+print(c3_data)
 
-        dimensions_arr = [{'Name': 'PayerAccountId', 'Value': payer},
-                         {'Name': 'ProductName', 'Value': svc['productname']}]
 
-        response = bch.get_total_month_to_date(payer, 'Services Month-to-Date', dimensions_arr)
+
