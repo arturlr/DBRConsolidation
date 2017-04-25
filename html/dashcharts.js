89c5f4e63964032f49cfdfd8d40b6036a1a7266f
artdev <arturlr@live.com>
2017-04-25 17:34:26 -0400

    v170425-1734

--- html/dashcharts.js
+++ html/dashcharts.js
@@ -1,14 +1,15 @@
 
-// Cognito Identity Pool Id
+// Initialize the Amazon Cognito credentials provider
+AWS.config.region = 'us-east-1';
 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-east-1:9f38c1fd-851b-4570-a517-fdc77227e14b'
+    IdentityPoolId: 'us-east-1:5308f314-dd80-4273-9130-2178df6e5dde',
 });
-AWS.config.region = 'us-east-1'; // Region
+
 
 // Cognito User Pool Id
 AWSCognito.config.region = 'us-east-1';
 AWSCognito.config.credentials = new AWS.CognitoIdentityCredentials({
-    IdentityPoolId: 'us-east-1:9f38c1fd-851b-4570-a517-fdc77227e14b'
+    IdentityPoolId: 'us-east-1:5308f314-dd80-4273-9130-2178df6e5dde'
 });
 
 AWSCognito.config.credentials.get();
@@ -17,9 +18,9 @@ AWSCognito.config.credentials.get();
 //console.log("AWSCognito.config.credentials"+JSON.stringify(AWSCognito.config.credentials));
 
 var COGNITO_REGION = 'us-east-1';
-var USER_POOL_ID = 'us-east-1_Q5V9smPLb';
-var IDENTITY_POOL_ID = 'us-east-1:9f38c1fd-851b-4570-a517-fdc77227e14b';
-var CLIENT_ID = '6lpap7cs0l1q28o2nnpnia8u7u';
+var USER_POOL_ID = 'us-east-1_RdBiutRhk';
+var IDENTITY_POOL_ID = 'us-east-1:5308f314-dd80-4273-9130-2178df6e5dde';
+var CLIENT_ID = '7b6ejd42btgrtip7nvm5vd2jm';
 
 var awstoken;
 
@@ -48,9 +49,69 @@ $(document).ready(function() {
         $('#id01').modal('toggle');
     });
 
+    $('#change_psw').on('click', function (e) {
+        $('#id03').modal('toggle');
+    });
+
+    $("body").on("click", "#fakeSubmit_initial_psw", function (e) {
+        e.preventDefault();
+        uname = $("#uname_initial_psw").val();
+        initialPassword = $("#initial_psw").val();
+        newPassword = $("#new_psw").val();
+
+        var authenticationData = {
+        Username : uname,
+        Password : initialPassword,
+        };
+        var authenticationDetails = new AWSCognito.CognitoIdentityServiceProvider.AuthenticationDetails(authenticationData);
+
+        var poolData = {
+            UserPoolId : USER_POOL_ID,
+            ClientId : CLIENT_ID
+        };
+
+        var userPool = new AWSCognito.CognitoIdentityServiceProvider.CognitoUserPool(poolData);
+        var userData = {
+            Username : uname,
+            Pool : userPool
+        };
+
+        var cognitoUser = new AWSCognito.CognitoIdentityServiceProvider.CognitoUser(userData);
+
+        cognitoUser.authenticateUser(authenticationDetails, {
+            onSuccess: function (result) {
+                // User authentication was successful
+            },
+
+            onFailure: function(err) {
+                sweetAlert("You probably messed something up!", err, "error");
+                // User authentication was not successful
+            },
+
+            mfaRequired: function(codeDeliveryDetails) {
+                // MFA is required to complete user authentication.
+                // Get the code from user and call
+                cognitoUser.sendMFACode(mfaCode, this)
+            },
+
+            newPasswordRequired: function(userAttributes, requiredAttributes) {
+                // User was signed up by an admin and must provide new
+                // password and required attributes, if any, to complete
+                // authentication.
+
+                // the api doesn't accept this field back
+                delete userAttributes.email_verified;
+                delete userAttributes.phone_number_verified;
+
+                // Get these details and call
+                cognitoUser.completeNewPasswordChallenge(newPassword, userAttributes, this);
+            }
+        });
+        $('#id03').modal('toggle');
+     })
+
     $("body").on("click", "#fakeSubmit", function (e) {
         e.preventDefault();
-        console.log('fake_login');
         $('#id01').modal('toggle');
         uname = $("#uname").val();
         pwd = $("#psw").val();
@@ -73,22 +134,22 @@ $(document).ready(function() {
       };
       var cognitoUser = new AWSCognito.CognitoIdentityServiceProvider.CognitoUser(userData);
 
-      console.log("AWS.config.credentials"+JSON.stringify(AWS.config.credentials));
-
-
-      //console.log('cognitoUser'+JSON.stringify(cognitoUser));
-      //console.log('userData'+JSON.stringify(userData));
       //console.log("AWS.config.credentials"+JSON.stringify(AWS.config.credentials));
-      //console.log("AWSCognito.config.credentials"+JSON.stringify(AWSCognito.config.credentials));
 
+/*
+      console.log('cognitoUser'+JSON.stringify(cognitoUser));
+      console.log('userData'+JSON.stringify(userData));
+      console.log("AWS.config.credentials"+JSON.stringify(AWS.config.credentials));
+      console.log("AWSCognito.config.credentials"+JSON.stringify(AWSCognito.config.credentials));
+*/
       try {
         cognitoUser.authenticateUser(authenticationDetails, {
             onSuccess: function (result) {
-                console.log('access token + ' + result.getAccessToken().getJwtToken());
+                //console.log('access token + ' + result.getAccessToken().getJwtToken());
 
                 var login = 'cognito-idp.' + COGNITO_REGION + '.amazonaws.com/' + USER_POOL_ID;
 
-                console.log('login string is: ' + login);
+                //console.log('login string is: ' + login);
 
                 AWS.config.credentials = new AWS.CognitoIdentityCredentials({
                     IdentityPoolId : IDENTITY_POOL_ID, // your identity pool id here
@@ -101,37 +162,32 @@ $(document).ready(function() {
                     // now using authenticated credentials
                     if(err)
                     {
+                      sweetAlert("Login Failed", err, "error");
                       console.log('Error in authenticating to AWS '+ err);
-
                     }
                     else
                     {
-                      console.log('identityId is: ' + AWS.config.credentials.identityId);
+                      //console.log('identityId is: ' + AWS.config.credentials.identityId);
                       awstoken = {
                         expireTime: AWS.config.credentials.expireTime,
                         accessKeyId: AWS.config.credentials.accessKeyId,
                         sessionToken: AWS.config.credentials.sessionToken,
                         secretAccessKey: AWS.config.credentials.secretAccessKey
                       };
-                      var table = new AWS.DynamoDB({params: {TableName: 'managelock'}});
-                      table.getItem({Key: {user: {S: $('#inputEmail').val()}}}, function(err, data) {
-                        //{"Item":{"thingName":{"S":"jodion-pi"},"user":{"S":"jondion@amazon.com"}}}
-                        console.log(data.Item.thingName.S); // print the item data
-                        thingName = data.Item.thingName.S;
-                        isLoggedIn = true;
 
-                        refreshStatus();
+                      RenderCharts();
 
-                      });
                     }
                 });
             },
 
             onFailure: function(err) {
+                sweetAlert("Login Failed", err, "error");
                 //alert(err);
             },
         });
       } catch(e) {
+          sweetAlert("Login Failed", err, "error");
           console.log(e);
       }
 
